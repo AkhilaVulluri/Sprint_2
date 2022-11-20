16d64aaa0f982b54ed61b84a0a2275a69929f8ad
AkhilaVulluri <101192229+AkhilaVulluri@users.noreply.github.com>
2022-11-20 16:15:36 +0530

    Create log_in.cpp

---
+++ Code/Client/log_in.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include "Logger.h"
+#include "header.h"
+using namespace std;
+
+void welcomeScreen ()
+{
+    cout << "\n\n\n\n";
+  cout << "\t\t\t===================================================\n";
+  cout << "\t\t\t||                  WELCOME TO                    ||\n";
+  cout << "\t\t\t||                                                ||\n";
+  cout << "\t\t\t|| Encrypted and concurrent socket implementation ||\n";
+  cout<<  "\t\t\t||    using gaming application (TIC-TAC-TOE GAME  ||\n";
+  cout<<  "\t\t\t||          -USING-CLIENT-SERVER-SOCKET)          ||\n";
+  cout << "\t\t\t||                                                ||\n";
+  cout << "\t\t\t||                    PROJECT                     ||\n";
+  cout << "\t\t\t===================================================\n\n\n";
+
+  
+  
+  
+     // declaration of varabiles
+     
+       string name;
+       char gender;
+       int age;
+       
+    //reading user name
+    
+    cout << "\n Enter User Name  :  ";
+    getline (cin,name);
+
+     cout<< "\n Enter User Age :";
+     cin>> age;
+    if(age >18)
+    {
+    Logger :: Info("valid age");
+    }
+    else
+    {
+    Logger :: Error("invalid input");
+    }
+    
+     //Reading gender from user
+
+     cout << "\n Enter gender (M/m or F/f ): ";
+     cin >> gender;
+       
+	if(gender =='m' || gender=='M' || gender=='f' || gender=='F'){
+
+	    Logger :: Info("Login successful");
+	}
+	else{
+		Logger :: Error("invalid input");
+	}
+
+}
