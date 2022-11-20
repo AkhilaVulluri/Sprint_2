81184104741617c7bac08e9c93488ac6fb1890f5
AkhilaVulluri <101192229+AkhilaVulluri@users.noreply.github.com>
2022-11-20 16:07:13 +0530

    Create instructions.cpp

---
+++ Code/Server/instructions.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include"header.h"
+using namespace std;
+
+
+void instruction(){
+
+    
+    
+    cout << "\n\n\n\n";
+  cout <<"\t\t\t**************************************************\n";
+  cout << "\t\t\t||                                             ||\n";
+  cout << "\t\t\t||          Instruction of the Game            ||\n";
+  cout << "\t\t\t||                                             ||\n";
+  cout << "\t\t\t||         RULES FOR TIC-TAC-TOE               ||\n";
+  cout << "\t\t\t||                                             ||\n";
+  cout <<"\t\t\t**************************************************\n";
+
+     cout<<"\n\n";
+     cout<< " 1. The game is played on a grid that s 3 squares by 3 squares. \n";
+
+     cout<< " 2. You are X, your friend (or the computer in this case) is O. \n"; 
+     cout<<"    Players take turns putting their marks in empty squares. \n";
+
+     cout<< " 3. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner. \n";
+
+     cout<< " 4. When all 9 squares are full, the game is over.\n";
+     cout<< "    If no player has 3 marks in a row, the game ends in a tie. \n";
+
+
+}
