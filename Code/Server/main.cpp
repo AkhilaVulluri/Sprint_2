1a91355a88095ab30a53221f30c4c8c68daa7f21
vignesh0909200 <116144442+vignesh0909200@users.noreply.github.com>
2022-11-21 15:27:00 +0530

    Update main.cpp

--- Code/Server/main.cpp
+++ Code/Server/main.cpp
@@ -8,9 +8,15 @@ int main(){
 
 
  welcomeScreen();
-  sleep(3);
+ sleep(3);
  instruction();
-
+ cout<<"THE GAME WILL START IN '3' SEC";
+  sleep(1);
+   cout<<'3';
+ sleep(1);
+   cout<<'2';
+ sleep(1);
+   cout<<'1';
 
 return 0;
 }
