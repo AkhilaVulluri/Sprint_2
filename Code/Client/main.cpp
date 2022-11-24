b1e00918d55da1bcd1907a4b6dcaaef655088936
AkhilaVulluri <101192229+AkhilaVulluri@users.noreply.github.com>
2022-11-24 08:53:22 +0530

    Update main.cpp

--- Code/Client/main.cpp
+++ Code/Client/main.cpp
@@ -1,3 +1,4 @@
+//Header Files
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
@@ -13,42 +14,58 @@ using namespace std;
 
 char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
+//Function to check the possible winning conditions
 int checkwin()
 {
     if (square[1] == square[2] && square[2] == square[3])
+	{
         return 1;
-        
+    }
     else if (square[4] == square[5] && square[5] == square[6])
+	{
         return 1;
-        
+    }   
     else if (square[7] == square[8] && square[8] == square[9])
+    {
         return 1;
-        
+    }   
     else if (square[1] == square[4] && square[4] == square[7])
+    {
         return 1;
+    }   
         
     else if (square[2] == square[5] && square[5] == square[8])
+    {
         return 1;
+    }   
         
     else if (square[3] == square[6] && square[6] == square[9])
+    {
         return 1;
+    }   
         
     else if (square[1] == square[5] && square[5] == square[9])
+    {
         return 1;
+    }   
         
     else if (square[3] == square[5] && square[5] == square[7])
+    {
         return 1;
-        
+    }   
     else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
         square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
         != '7' && square[8] != '8' && square[9] != '9')
-
+	{
         return 0;
+	}
     else
+	{
         return  - 1;
+	}
 }
 
-
+//Display of Game Board
 void board()
 {
 
@@ -73,7 +90,7 @@ void board()
     cout<<"\t\t    |     |     \n";
 }
 
-
+//Main Function
 int main()
 {
 	welcomeScreen();
@@ -90,21 +107,23 @@ int main()
 		FILE *filep;
 		filep=fopen("score.txt","aopen+");
 		fclose(filep);
-		int player=1;
-		char symbol;
+		//int player=1;
+		//char symbol;
 		int ccleintid;
-		char str[100];
-		int clientid;
+		//char str[100];
+		//int clientid;
 		int msgbyte,cho;
 		int choice,aopen,schoice;
 		int icount;
 		char mark;
 		stringstream ss;
 		
-	   // rules();
+	   //Selection of options to proceed into the game 
 	   
 		cout<<"\n\nType 1 to start the game:-\nType 2 to view leader board:-\nType 3 to exit:-\n";
 		cin>>schoice;
+		
+		// Display of Leaderboard
 		if(schoice==2)
 		{
 		leader:
@@ -125,15 +144,25 @@ int main()
 			cout<<"\n";
 			cin>>cho;
 			if(cho==1)
+			{
 				goto jump;
+			}
 			else if(cho==2)
+			{
 				goto leader;
+			}
 			else if(cho==3)
+			{
 				goto jump2;
+			}
 			else 
+			{
 				getchar();
+			}
 			
 		}
+		
+		//Condition to exit the game
 		if(schoice==3)
 		{
 		jump2:
@@ -144,6 +173,7 @@ int main()
 			return 0;
 		}
 		
+		//Condition to iterate the game
 		if(schoice==1)
 		{
 			int port;
@@ -188,31 +218,48 @@ int main()
 					
 					mark='X';
 					if (choice == 1 && square[1] == '1')
+					{
 						square[1] = mark;
-						
+					}	
 					else if (choice == 2 && square[2] == '2')
+					{
 						square[2] = mark;
+					}
 						
 					else if (choice == 3 && square[3] == '3')
+					{
 						square[3] = mark;
+					}
 						
 					else if (choice == 4 && square[4] == '4')
+					{
 						square[4] = mark;
+					}
 						
 					else if (choice == 5 && square[5] == '5')
+					{
 						square[5] = mark;
+					}
 						
 					else if (choice == 6 && square[6] == '6')
+					{
 						square[6] = mark;
+					}
 						
 					else if (choice == 7 && square[7] == '7')
+					{
 						square[7] = mark;
+					}
 						
 					else if (choice == 8 && square[8] == '8')
+					{
 						square[8] = mark;
+					}
 						
 					else if (choice == 9 && square[9] == '9')
+					{
 						square[9] = mark;
+					}
 						
 					
 					msgbyte=send(ccleintid,&choice,sizeof(choice),0);
@@ -246,43 +293,69 @@ int main()
 						cout<<"\n";
 						cin>>cho;
 						if(cho==1)
+						{
 							goto jump;
+						}
 						else if(cho==2)
+						{
 							goto leader;
+						}
 						else if(cho==3)
+						{
 							goto jump2;
+						}
 						else 
+						{
 							getchar();
+						}
 						break;
 						
 					}
 					mark='O';
 					if (choice == 1 && square[1] == '1')
+						{
 							square[1] = mark;
+						}
 						
 						else if (choice == 2 && square[2] == '2')
+						{
 							square[2] = mark;
+						}
 						
 						else if (choice == 3 && square[3] == '3')
+						{
 							square[3] = mark;
+						}
 						
 						else if (choice == 4 && square[4] == '4')
+						{
 							square[4] = mark;
+						}
 						
 						else if (choice == 5 && square[5] == '5')
+						{
 							square[5] = mark;
+						}
 						
 						else if (choice == 6 && square[6] == '6')
+						{
 							square[6] = mark;
+						}
 						
 						else if (choice == 7 && square[7] == '7')
+						{
 							square[7] = mark;
+						}
 							
 						else if (choice == 8 && square[8] == '8')
+						{
 							square[8] = mark;
+						}
 						
 						else if(choice == 9 && square[9] == '9')
+						{
 							square[9] = mark;
+						}
 						
 
 
@@ -316,13 +389,21 @@ int main()
 						cout<<"\n";
 						cin>>cho;
 						if(cho==1)
+						{
 							goto jump;
+						}
 						else if(cho==2)
+						{
 							goto leader;
+						}
 						else if(cho==3)
+						{
 							goto jump2;
+						}
 						else 
+						{
 							getchar();
+						}
 						break;
 						
 					}
