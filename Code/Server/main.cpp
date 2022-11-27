1de3e4c89bf00055ae39ef25754e75ba86a8ea5b
AkhilaVulluri <101192229+AkhilaVulluri@users.noreply.github.com>
2022-11-27 18:27:37 +0530

    Update main.cpp

--- Code/Server/main.cpp
+++ Code/Server/main.cpp
@@ -10,9 +10,21 @@
 #include<iostream>
 #include "header.h"
 #define MAX 10
+#define BACKLOG 5
+#define INDEX_0 '0'
+#define INDEX_1 '1'
+#define INDEX_2 '2'
+#define INDEX_3 '3'
+#define INDEX_4 '4'
+#define INDEX_5 '5'
+#define INDEX_6 '6'
+#define INDEX_7 '7'
+#define INDEX_8 '8'
+#define INDEX_9 '9'
+
 using namespace std;
 
-char square[MAX] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[MAX] = { INDEX_0, INDEX_1, INDEX_2, INDEX_3, INDEX_4, INDEX_5, INDEX_6, INDEX_7, INDEX_8, INDEX_9 };
 
 //Function to check the possible winning conditions
 int checkwin()
@@ -119,26 +131,27 @@ int main()
 
 
 		
-	int serverid;
-	//File open
+	int server_file_discriptor;
 	FILE *filep;
 	filep=fopen("score.txt","aopen+");
 	fclose(filep);
-	
+	//int player=1;
 	char symbol;
-	int cho;
+	//int clientsideid;
+	//char str[100];
+	int Inner_choice;
 	//int cid;
 	int msgbyte;
-	int choice,aopen,schoice;
+	int choice,aopen,Main_choice;
 	int icount;
 	char mark;
 		
-	  
+	 
 	   
 	cout<<"\n\nType 1 to start the game:-\nType 2 to view leader board:-\nType 3 to exit:-\n";
-	cin>>schoice;
+	cin>>Main_choice;
 		
-		if(schoice==2)
+		if(Main_choice==2)
 		{
 		leader:
 			cout<<"\n\n";
@@ -156,16 +169,16 @@ int main()
 			cout<<"\n\nPress 2 to see the Leaderboard";
 			cout<<"\n\nPress 3 to Exit:-";
 			cout<<"\n";
-			cin>>cho;
-			if(cho==1)
+			cin>>Inner_choice;
+			if(Inner_choice==1)
 			{
 				goto jump;
 			}
-			else if(cho==2)
+			else if(Inner_choice==2)
 			{
 				goto leader;
 			}
-			else if(cho==3)
+			else if(Inner_choice==3)
 			{
 				goto jump2;
 			}
@@ -176,50 +189,56 @@ int main()
 		}
 		
 		//Condition to exit the game
-		if(schoice==3)
+		if(Main_choice==3)
 		{
 		jump2:
 			cout<<"Exit\n";
 			cout<<"THANK U FOR PLAYING THE GAME \n";
 			cout<<"HAVE A NICE DAY..........!!!!!\n";
-			close(serverid);
+			close(server_file_discriptor);
 			return 0;
 		}
 		
 		//Condition to iterate the game
-		if(schoice==1)
+		if(Main_choice==1)
 		{
 		int sid,port;
 		port =6999;
 		jump:
 			
-			serverid=socket(AF_INET,SOCK_STREAM,0);
-			if(serverid==-1)
+			//Connection of Server Side
+			
+			server_file_discriptor=socket(AF_INET,SOCK_STREAM,0);
+			if(server_file_discriptor==-1)
 			{
 				cout<<"IN SOCKET CREATION IN SERVER SIDE FAILURE\n";
 				exit(0);
 			}
 			cout<<"IN SOCKET CREATION IN SERVER SIDE SUCCESSFUL\n";
 			port+=1;
-			struct sockaddr_in ssadd;
-			ssadd.sin_family=AF_INET;
-			ssadd.sin_port=htons(port);
-			ssadd.sin_addr.s_addr=inet_addr("10.0.2.15");
-			sid=bind(serverid,(struct sockaddr*)&ssadd,sizeof(ssadd));
-			if(sid==-1)
+			
+			//Assiging the Struct Attributes
+			struct sockaddr_in server_sock_address;
+			server_sock_address.sin_family=AF_INET;
+			server_sock_address.sin_port=htons(port);
+			server_sock_address.sin_addr.s_addr=inet_addr("10.0.2.15");
+			
+			//Binding the Ports
+			bind_file_descriptor=bind(server_file_discriptor,(struct sockaddr*)&server_sock_address,sizeof(server_sock_address));
+			if(bind_file_descriptor==-1)
 			{
 				cout<<"\nBINDING FAILURE AT SERVER SIDE\n";
 				exit(0);
 			}
 			cout<<"\nBINDING SUCCESSFUL AT SERVER SIDE\n";
 
-			listen(serverid,5);
-
-
+			listen(server_file_discriptor,BACKLOG);
+			
+			//Accepting the Connection
 			struct sockaddr_in fadd;
 				socklen_t  len=sizeof(fadd);
-			int fd1=accept(serverid,(struct sockaddr*)&fadd,&len);
-			if(fd1==(-1))
+			int accept_file_descriptor=accept(server_file_discriptor,(struct sockaddr*)&fadd,&len);
+			if(accept_file_descriptor==(-1))
 			{
 				cout<<"\nFAILURE AT SERVER SIDE IN ACCEPT";
 				exit(0);
@@ -229,11 +248,13 @@ int main()
 			
 			while(icount!=1)
 			{
-				msgbyte=recv(fd1,&choice,sizeof(choice),0);
+			
+				//Reciving the Message from Client Side
+				msgbyte=recv(accept_file_descriptor,&choice,sizeof(choice),0);
 				filep=fopen("score.txt","aopen+");
 				if(choice==11)
 				{
-					cout<<"You win";
+					cout<<"Client Win";
 					square[1] = '1';
 					square[2] = '2';
 					square[3] = '3';
@@ -243,9 +264,9 @@ int main()
 					square[7] = '7';
 					square[8] = '8';
 					square[9] = '9';
-					close(serverid);
+					close(server_file_discriptor);
 						cout<<"\n";
-						fprintf(filep,"\t\nServer\n");
+						fprintf(filep,"\t\nClient\n");
 						cout<<"\n";
 						getchar();
 						fclose(filep);
@@ -253,16 +274,16 @@ int main()
 						cout<<"\n\nPress 2 to see the Leaderboard";
 						cout<<"\n\nPress 3 to Exit:-";
 						cout<<"\n";
-						cin>>cho;
-						if(cho==1)
+						cin>>Inner_choice;
+						if(Inner_choice==1)
 						{
 							goto jump;
 						}
-						else if(cho==2)
+						else if(Inner_choice==2)
 						{
 							goto leader;
 						}
-						else if(cho==3)
+						else if(Inner_choice==3)
 						{
 							goto jump2;
 						}
@@ -323,7 +344,7 @@ int main()
 				if(icount==1)
 				{	board();
 					int flag=10;
-					cout<<"client win";
+					cout<<"you win";
 					square[1] = '1';
 					square[2] = '2';
 					square[3] = '3';
@@ -333,27 +354,27 @@ int main()
 					square[7] = '7';
 					square[8] = '8';
 					square[9] = '9';
-					close(serverid);
+					close(server_file_discriptor);
 					cout<<"\n";
-						fprintf(filep,"\t\nClient\n");
+						fprintf(filep,"\t\nServer\n");
 						cout<<"\n";
 						getchar();
 						fclose(filep);
-						msgbyte=send(fd1,&flag,sizeof(flag),0);
+						msgbyte=send(accept_file_descriptor,&flag,sizeof(flag),0);
 						cout<<"\n\nPress 1 to Play Again:- ";
 						cout<<"\n\nPress 2 to see the Leaderboard";
 						cout<<"\n\nPress 3 to Exit:-";
 						cout<<"\n";
-						cin>>cho;
-						if(cho==1)
+						cin>>Inner_choice;
+						if(Inner_choice==1)
 						{
 							goto jump;
 						}
-						else if(cho==2)
+						else if(Inner_choice==2)
 						{
 							goto leader;
 						}
-						else if(cho==3)
+						else if(Inner_choice==3)
 						{
 							goto jump2;
 						}
@@ -421,14 +442,14 @@ int main()
 					square[9] = mark;
 				}
 					
-				
-				msgbyte=send(fd1,&choice,sizeof(choice),0);
+				//Sending Message to Client Side
+				msgbyte=send(accept_file_descriptor,&choice,sizeof(choice),0);
 			
 			}
 		
 		
 		}
-	close(serverid);
+	close(server_file_discriptor);
 	return 0;
  
 }
